f78d29e3ce6f857553ed53ceb5dfc8eef3e9ff09
casperwang <casperwang2014@gmail.com>
2020-05-21 16:42:57 +0800

    update

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import init
 
 def go(target):
     if target == 'battle':
-        pyautogui.moveTo(500, 780, duration=-1000)
+        pyautogui.moveTo(1127, 1156, duration=-1000)
         pyautogui.mouseDown()
         pyautogui.mouseUp()
     if target == 'duel':
@@ -12,17 +12,16 @@ def go(target):
 
 def getBall(round, ball):
 	ballnum = 7 - round
-	return (500 + (ball - (ballnum + 1) / 2) * 60, 870)
+	return (1127 + (ball - (ballnum + 1) / 2) * 85, 1294)
 
 def pokemon_go(r, b):
 	x, y = getBall(r, b)
 	pyautogui.moveTo(x, y)
-	pyautogui.click()
-	pyautogui.click()
-	pyautogui.drag(0, -200, button='left')
+	pyautogui.mouseDown()
+	pyautogui.moveTo(1127, 1016, duration=-1000)
+	pyautogui.mouseUp()
 
 def battle():
-    init.screen_size()
     go('battle')
     time.sleep(20)
     pokemon_go(1, 4)
