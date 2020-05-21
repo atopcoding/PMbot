7b446784cf466424c5f6a367e3986a538d181548
xSeanliux <zhxnliu@gmail.com>
2020-05-21 15:50:41 +0800

    Add init.py

---
+++ init.py
@@ -0,0 +1,23 @@
+import pyautogui
+
+#Resize screen to fit 1000 * 1000
+screenWidth, screenHeight = pyautogui.size()
+pyautogui.moveTo(screenWidth - 5, screenHeight - 5)
+pyautogui.click()
+pyautogui.dragTo(1000, 1000, button = 'left')
+
+#center at x = 500, y = 870, width = 60
+def getBall(round, ball):
+	ballnum = 7 - round
+	return (500 + (ball - (ballnum + 1) / 2) * 60, 870)
+
+
+def go():
+	r = int(input())
+	b = int(input())
+	x, y = getBall(r, b)
+	pyautogui.moveTo(x, y)
+	pyautogui.click()
+	pyautogui.click()
+	pyautogui.drag(0, -200, button = 'left')
+	
