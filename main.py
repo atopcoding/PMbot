fdcb37cae0e06174c8f9a9259ffbbdc81d4bb937
xSeanliux <zhxnliu@gmail.com>
2020-05-21 16:38:49 +0800

    Add stuff

--- main.py
+++ main.py
@@ -1,36 +1,34 @@
 import time
+from variables import *
 import pyautogui
-import init
 
 def go(target):
     if target == 'battle':
-        pyautogui.moveTo(500, 780, duration=-1000)
-        pyautogui.mouseDown()
-        pyautogui.mouseUp()
-    if target == 'duel':
+        pyautogui.moveTo(1123, 1145, duration=-1000)
+        pyautogui.click()
+    elif target == 'duel':
         pyautogui.moveTo(876, 1144, duration=-1000)
+    elif target == 'ad':
+        pyautogui.moveTo(400, 840, duration=-1000)
 
-def getBall(round, ball):
-	ballnum = 7 - round
-	return (500 + (ball - (ballnum + 1) / 2) * 60, 870)
 
-def pokemon_go(r, b):
-	x, y = getBall(r, b)
-	pyautogui.moveTo(x, y)
-	pyautogui.click()
-	pyautogui.click()
-	pyautogui.drag(0, -200, button='left')
+def getBall(round, id):
+    balls = 7 - round
+    return (ballCtr + ballDist * (id - (balls + 1) / 2), ballY)
+
+def pokemon_go(round, id):
+    x, y = getBall(round, id)
+    pyautogui.moveTo(x, y)
+    pyautogui.drag(0, -100, button = 'left')
 
 def battle():
-    init.screen_size()
     go('battle')
     time.sleep(20)
-    pokemon_go(1, 4)
+    print("Going to do move")
+    pokemon_go(1,4)
     time.sleep(20)
-    pokemon_go(2, 1)
+    print("Going to do move")
+    pokemon_go(2,3)
+    print("Going to do move")
     time.sleep(20)
-    pokemon_go(3, 2)
-    time.sleep(20)
-    pokemon_go(4, 3)
-    time.sleep(20)
-    pokemon_go(5, 2)
+    pokemon_go(3,2)
